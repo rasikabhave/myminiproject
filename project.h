d4c147c603b7726600e59ccb08d0b668adc1d24f
rasikabhave <bhaverp14.comp@coep.ac.in>
2015-11-15 17:23:31 +0530

    Update project.h

--- project.h
+++ project.h
@@ -2,9 +2,10 @@
 #define QUEEN 4
 #define XMAX 48
 #define YMAX 24
-#define STRTBY 4
+#define STRTBY 3
 #define STRTBX 8
 #define KING 5
+int board[8][8];
 typedef struct board_position{
 	int row,col;
 }bpos;
@@ -15,7 +16,22 @@ typedef struct lastmove{
 	int sx,sy,ex,ey;
 	int piece;
 } lastmove;
+typedef struct moves{
+	int srow,scol,erow,ecol,prevval,newval;
+	float c;
+}moves;
+typedef struct mstack{
+	int top;
+	moves arr[2];
+}mstack;
+typedef struct checkm{
+	int x,y,piece;
+}checkm;
+
 lastmove umove; 
+void quitgame();
+void display_menu();
+void printelement();
 int board[8][8],count1,count2,killed;
 bpos *position(int x,int y) ;
 void undo();
@@ -34,5 +50,4 @@ int horse(int x,int y,int x1,int y1);
 int king(int x,int y,int x1,int y1);
  int bishop(int x,int y,int x1,int y1);
 int queen(int x,int y,int x1,int y1);
-int rook(int x,int y,int x1,int y1);
-void check();
+int rook(int x,int y,int x1,int y1);void check();
